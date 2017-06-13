174f9271fd79bb10380aec26085cf5e3112f81fa
Jan Weidner <jw3126@gmail.com>
2017-06-13 23:24:03 +0200

    replace d

--- src/locate.jl
+++ src/locate.jl
@@ -15,17 +15,15 @@ function recipe_files(dir)
 end
 
 immutable RecipeLocation
-    path::String
     startline::Int64
     stopline::Int64
-    function RecipeLocation(path, startline, stopline)
+    function RecipeLocation(startline, stopline)
         @assert startline <= stopline
-        new(path, startline, stopline)
+        new(startline, stopline)
     end
 end
 
 type State
-    path::String
     searching_recipe_end::Bool
     current_recipe_start_line::Int
     current_line::Int
@@ -36,7 +34,7 @@ searching_recipe_end(s::State) = s.searching_recipe_end
 searching_recipe_end!(s, b) = (s.searching_recipe_end = b; b)
 function finish_recipe!(s::State)
     @assert searching_recipe_end(s)
-    ret = RecipeLocation(s.path, s.current_recipe_start_line, s.current_line - 1)
+    ret = RecipeLocation(s.current_recipe_start_line, s.current_line - 1)
     s.current_recipe_start_line = OUTSIDE_LINE
     searching_recipe_end!(s, false)
     ret
@@ -47,7 +45,7 @@ function start_recipe!(s::State)
     s.current_recipe_start_line = s.current_line
     s
 end
-State(path) = State(path, false, OUTSIDE_LINE, 1)
+State() = State(false, OUTSIDE_LINE, 1)
 
 locate_recipes!(ret, ex, state) = ret
 function locate_recipes!(ret, ex::Expr, state)
@@ -68,31 +66,76 @@ function locate_recipes!(ret, ex::Expr, state)
     ret
 end
 
-function locate_recipes(path)
+function locate_recipes(s::String)
     ret = RecipeLocation[]
-    state = State(path)
-    ex = parse("begin $(readstring(path)) end")
+    state = State()
+    ex = parse("begin $s end")
     locate_recipes!(ret, ex, state)
     if searching_recipe_end(state)
-        state.current_line = length(readlines(path)) + 1
+        state.current_line = length(split(s, '\n')) + 1
         push!(ret, finish_recipe!(state))
     end
     @assert !searching_recipe_end(state)
     ret
 end
     
-function recipe_lines(path)
-    recipes = locate_recipes(path)
+function recipe_lines(s)
     lines = Int64[]
-    for recipe in locate_recipes(path)
+    for recipe in locate_recipes(s)
         append!(lines, recipe.startline:recipe.stopline)
     end
-    # Set(lines)
     lines
 end
 
+function replace_token_in_recipes(s, pattern, r)
+    index = recipe_lines(s)
+    lines = String[]
+    for (i, line) in enumerate(String.(split(s, '\n')))
+        if i in index
+            line_new = ""
+            for token in tokenize(line)
+                if string(token) == pattern
+                    line_new *= r
+                else
+                    line_new *= string(token)
+                end
+            end
+            if line_new != line
+                println(i)
+                println(line)
+                println(line_new)
+            end
+            push!(lines, line_new)
+        else 
+            push!(lines, line)
+        end
+    end
+    join(lines, '\n')
+end
+
+function replace_token_in_recipes_dir(dir, pattern, r)
+    for path in recipe_files(dir)
+        s = readstring(path)
+        @show path
+        s_new = replace_token_in_recipes(s, pattern, r)
+        write(path, s_new)
+    end
+end
+
+function replace_d_pkg(pkg)
+    # Pkg.checkout(pkg)
+    dir = Pkg.dir(pkg)
+    run(Cmd(`git branch replaceD`, dir=dir))
+    run(Cmd(`git checkout replaceD`, dir=dir))
+    pattern = "d"
+    r = "plotattributes"
+    replace_token_in_recipes_dir(dir, pattern, r)
+    msg = "replace d by plotattributes inside recipes"
+    run(Cmd(`git commit -am $msg`, dir=dir))
+end
+
 function search_token_in_recipes(f, path)
-    index = recipe_lines(path)
+    index = recipe_lines(readstring(path))
     for (i,line) in enumerate(readlines(path))
         if i in index
             for token in tokenize(line)
--- src/main.jl
+++ src/main.jl
@@ -1,27 +1,33 @@
 include("util.jl")
 include("locate.jl")
 
-pkgs = sort!(unique(mapreduce(Pkg.dependents, vcat, ["Plots", "RecipesBase"])))
+p = "StatPlots"
+p = "Plots"
+p = "PlotRecipes"
 
-const PKGDB = joinpath(Pkg.dir("ReplaceD"), "pkgdb")
-ispath(PKGDB) || mkpath(PKGDB)
-pkgdbpath(p) = joinpath(PKGDB, p)
+replace_d_pkg(p)
 
-for p in pkgs
-    target = pkgdbpath(p)
-    if !ispath(target)
-        cmd = `git clone $(url(p)) $target`
-        run(cmd)
-    end
-end
-
-
-for pkg in pkgs
-    root = pkgdbpath(pkg)
-    println(pkg)
-    for path in recipe_files(root)
-        # parse("begin $(readstring(path)) end")
-        println(" "^4, splitdir(path)[2])
-        search_token_in_recipes(tok -> string(tok) == "d", path)
-    end
-end
+# pkgs = sort!(unique(mapreduce(Pkg.dependents, vcat, ["Plots", "RecipesBase"])))
+# 
+# const PKGDB = joinpath(Pkg.dir("ReplaceD"), "pkgdb")
+# ispath(PKGDB) || mkpath(PKGDB)
+# pkgdbpath(p) = joinpath(PKGDB, p)
+# 
+# for p in pkgs
+#     target = pkgdbpath(p)
+#     if !ispath(target)
+#         cmd = `git clone $(url(p)) $target`
+#         run(cmd)
+#     end
+# end
+# 
+# 
+# for pkg in pkgs
+#     root = pkgdbpath(pkg)
+#     println(pkg)
+#     for path in recipe_files(root)
+#         # parse("begin $(readstring(path)) end")
+#         println(" "^4, splitdir(path)[2])
+#         search_token_in_recipes(tok -> string(tok) == "d", path)
+#     end
+# end
